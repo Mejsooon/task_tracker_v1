e45da753f8877744bd5db880021610c1dad5b3a6
Mikołaj <mikolajfrak.priv@gmail.com>
2026-04-17 23:08:12 +0200

    Added get_next_id function

--- app/utils/helpers.py
+++ app/utils/helpers.py
@@ -1,4 +1,15 @@
-import os
-
 def clear_screen():
     print("\n" * 10)
+
+
+def get_next_id(prefix: str, items: list):
+    if not items:
+        return f"{prefix}001"
+
+    only_numbers = []
+    for item in items:
+        if item.startswith(prefix):
+            only_numbers.append(int(item[1:])) # Wycinam pierwszy znak (U) i zwracam samą liczbę
+
+    max_number = max(only_numbers)
+    return f"{prefix}{max_number + 1:03d}"

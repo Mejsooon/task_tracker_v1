ab95893c50458e71ab71357bfcd4272b5335a34a
Mikołaj <mikolajfrak.priv@gmail.com>
2026-04-17 23:07:17 +0200

    Added save user function

--- app/repositories/user_repository.py
+++ app/repositories/user_repository.py
@@ -5,3 +5,7 @@ from typing import Optional
 
 def find_by_username(username: str) -> Optional[User]:
     return next((u for u in store.users if u.username == username), None)
+
+def save(user: User):
+    if user not in store.users:
+        store.users.append(user)
